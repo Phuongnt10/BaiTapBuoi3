4f581a24cc9117c3c6477f1477fe068e71d9242d
Phuongnt <phuong.nguyen10@onemount.com>
2021-09-20 20:46:55 +0700

    update file name

--- regex.py
+++ regex.py
@@ -1,9 +1,3 @@
 import re
 list_resutl=[]
-t= open('animal_code.google (1).com',encoding='UTF-8')
-tf=t.read()
-find_add=re.findall("GET (.*\.jpg).*/(.\..\..\..)",tf)
-find_dup=set (find_add)
-for i in find_dup:
-    join="http://"+i[1]+i[0]
-    print(join)
+t= open('animal_code.google.com',encoding='UTF-8')
