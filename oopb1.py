d21c908691b9c877246a4aaba407ae05b91d2fe1
Phuongnt <phuong.nguyen10@onemount.com>
2021-10-09 15:30:58 +0700

    count

--- oopb1.py
+++ oopb1.py
@@ -3,30 +3,37 @@ class BankAccount:
         self._balance=balance
         self._account_number=account_number
         self._account_name=account_name
+
+    @property  
     def get_account_number(self):
         return self._account_number
+    
+    @property      
     def  get_account_name(self):
         return self._account_name
-    def get_balance(self):
+    @property    
+    def balance(self):
         return self._balance   
 
-    def set_balance(self,new_balance):
-        if new_balance >=0:
-            self._balance=new_balance
+    @balance.setter
+    def balance(self,balance):
+        if balance >=0:
+            self._balance=balance
         else:
              print("Số dư không hợp lệ")   
     def display(self):
-       print("Thông tin tài khoản là:",f"{self.get_account_number()}  {self.get_account_name()}  {self.get_balance()}")
+       ##print("Thông tin tài khoản là:",f"{self.get_account_number()}  {self.get_account_name()}  {self.balance()}")
+        print(self.get_account_number, self.get_account_name, self.balance)
 
     def  withdraw(self, amount):
-        if 0 < amount < self._balance:
-            self._balance-=amount
+        if 0 < amount < self.balance:
+            self.balance-=amount
         else:
             print("Số dư không không đủ") 
 
     def deposit(self, amount):
         if amount >0:
-            self._balance+=amount
+            self.balance+=amount
         else:
             print("Số dư không hợp lệ") 
 
