fd19783eec5b1ea7d6845d189fdd9358b97e4bf8
Phuongnt10 <84447422+Phuongnt10@users.noreply.github.com>
2021-10-28 22:19:35 +0700

    update

--- hackathon1_midterm/medium.py
+++ hackathon1_midterm/medium.py
@@ -10,7 +10,7 @@ def anagram_number(n):
 
 def roman_to_int(s):
     
-    romans = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000,}
+    romans = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
     result = 0
     i = 0
     while i < len(s):
