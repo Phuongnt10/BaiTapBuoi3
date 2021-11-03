1c7ec14c52ec5469e4c704a20365d0ca1f49b68d
Phuongnt10 <84447422+Phuongnt10@users.noreply.github.com>
2021-11-03 22:29:59 +0700

    update

--- oopb1.py
+++ oopb1.py
@@ -41,3 +41,54 @@ bank=BankAccount('19001919','Nguyen Thi Phuong',10000000)
 bank.withdraw(500)
 bank.deposit(3000000)
 bank.display()
+
+
+
+'''
+class BankAccount:
+    def __init__(self,account_number, account_name, balance=0):
+        
+        self._account_number=account_number
+        self._account_name=account_name
+        self.set_balance(balance)
+
+    
+    def get_account_number(self):
+        return self._account_number
+    
+      
+    def  get_account_name(self):
+        return self._account_name
+    
+    def get_balance(self):
+        return self._balance   
+
+    def set_balance(self,balance):
+        if balance >=0:
+            self._balance=balance
+        else:
+             print("Số dư không hợp lệ")   
+
+    def  withdraw(self, amount):
+        if 0 < amount < self._balance:
+            self._balance-=amount
+        else:
+            print("Số dư không không đủ") 
+
+    def deposit(self, amount):
+        if amount >0:
+            self._balance+=amount
+        else:
+            print("Số dư không hợp lệ")          
+    def display(self):
+       print("Thông tin tài khoản là:",f"{self.get_account_number()}  {self.get_account_name()}  {self.get_balance()}")
+        # print(self.get_account_number, self.get_account_name, self.get_balance)
+
+    
+
+bank=BankAccount('19001919','Nguyen Thi Phuong',10000000)
+bank.withdraw(500)
+bank.deposit(3000000)
+bank.display()
+
+'''
